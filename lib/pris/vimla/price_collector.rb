b7ca782e75388e5060cfe42e3b3100cfbdad9d68
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-11-12 17:01:35 +0100

    Use UrlFetcher and DataFetcher to get data.

--- lib/pris/vimla/price_collector.rb
+++ lib/pris/vimla/price_collector.rb
@@ -3,6 +3,16 @@ module Pris
     class PriceCollector
 
       def countries
+        
+      end
+
+      def data
+        @data ||= get_data
+      end
+
+      def get_data
+        url = UrlFetcher.new.url
+        DataFetcher.new(url).data
       end
 
     end
