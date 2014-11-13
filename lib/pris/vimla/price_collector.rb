79fea437169c0e1949a64ed922f7f68d5b148aed
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-11-13 07:04:18 +0100

    Start specing and implementing the price coll

--- lib/pris/vimla/price_collector.rb
+++ lib/pris/vimla/price_collector.rb
@@ -3,16 +3,28 @@ module Pris
     class PriceCollector
 
       def countries
-        
+        @countries ||= collect_rates(raw_data)
       end
 
-      def data
-        @data ||= get_data
+      def raw_data
+        @raw_data ||= get_data
       end
 
       def get_data
         url = UrlFetcher.new.url
         DataFetcher.new(url).data
+      end
+
+      def collect_rates(raw_data)
+        countries = raw_data["countries"]
+        zones = raw_data["zones"]
+        countries.each_with_object({}) do |(country, data), collection|
+          collection[country] = build_country(data, zones)
+        end
+      end
+
+      def build_country(country, zones)
+        
       end
 
     end
--- spec/vimla/price_collector_spec.rb
+++ spec/vimla/price_collector_spec.rb
@@ -4,9 +4,23 @@ module Pris
   module Vimla
     describe PriceCollector do
       let(:collector) { PriceCollector.new }
+      let(:example_data) { Oj.load File.open("./spec/example_data/vimla.json") { |f| f.read} }
+      let(:example_countries) { example_data["countries"].keys }
+
       describe "#countries" do
         it "returns all countries" do
-          expect(collector.countries.length).to eq(207)
+          expect(collector.countries.keys).to eq(example_countries)
+        end
+
+        it "returns the correct data schema" do
+          country = collector.countries["Finland"]
+          expected = {
+            to: 2.5,
+            from: 0.67,
+            within: 0.67,
+            receive: 0
+          }
+          expect(country.sms).to eq(expected)
         end
       end
     end
