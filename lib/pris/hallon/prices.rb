82f9ecf5038306a71c8aed1257e803bf2b649189
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-11-19 09:50:34 +0100

    Use new DataFetcher to fetch countries on demand instead of in bulk

--- lib/pris/hallon/prices.rb
+++ lib/pris/hallon/prices.rb
@@ -2,24 +2,40 @@ module Pris
   module Hallon
     class Prices < PriceSource
       def countries
-        @countries ||= construct_countries(raw_data)
+        @countries ||= CountriesProxy.new(urls)
       end
 
-      def raw_data
-        @raw_data ||= begin
-          urls = UrlFetcher.new.urls
-          DataFetcher.new(urls).data
+      def urls
+        @urls ||= UrlFetcher.new.urls
+      end
+
+      class CountriesProxy
+        def initialize(urls)
+          @urls = urls
+          @countries = {}
+        end
+
+        def [](country)
+          countries[countries] || get_country(country)
+        end
+
+        private
+
+        attr_reader :countries
+
+        Country = Struct.new(:rates)
+
+        def get_country(country)
+          if url = url_for(country)
+            data = DataFetcher.new(url).data
+            countries[country] = Country.new(Rates.from_hash(data))
+          end
+        end
+
+        def url_for(country)
+          @urls[country]
         end
       end
-
-      def construct_countries(raw_data)
-        raw_data.each_with_object({}) do |(country, rates), all_rates|
-          rates = Rates.new(rates)
-          all_rates[country] = Country.new(rates)
-        end
-      end
-
-      Country = Struct.new(:rates)
     end
   end
 end
--- spec/hallon/prices_spec.rb
+++ spec/hallon/prices_spec.rb
@@ -10,40 +10,40 @@ module Pris
 
         it "returns the correct prices to Finland" do
           expected = {
-              "phone" => 0.39,
-              "mobile" => 1.45,
-              "sms" => 0,
-              "mms" => 3.5
+              phone: 0.39,
+              mobile: 1.45,
+              sms: 0,
+              mms: 3.5
           }
           expect(country.rates.to).to eq(expected)
         end
 
         it "returns the correct prices from Finland" do
           expected = {
-              "phone" => 2.11,
-              "mobile" => 2.11,
-              "sms" => 0.66,
-              "mms" => 2.22
+              phone: 2.11,
+              mobile: 2.11,
+              sms: 0.66,
+              mms: 2.22
           }
           expect(country.rates.from).to eq(expected)
         end
 
         it "returns the correct prices within Finland" do
           expected = {
-              "phone" => 2.11,
-              "mobile" => 2.11,
-              "sms" => 0.66,
-              "mms" => 2.22,
-              "data" => 2.22
+              phone: 2.11,
+              mobile: 2.11,
+              sms: 0.66,
+              mms: 2.22,
+              data: 2.22
           }
           expect(country.rates.within).to eq(expected)
         end
 
         it "returns the correct prices for receiving in Finland" do
           expected = {
-              "mobile" => 0.55,
-              "sms" => 0,
-              "mms" => 0
+              mobile: 0.55,
+              sms: 0,
+              mms: 0
           }
           expect(country.rates.receive).to eq(expected)
         end
