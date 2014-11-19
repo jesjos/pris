b4da803995d9daac82094ef09883df9315d4780c
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-11-19 09:49:53 +0100

    Rates should have symbolized keys

--- lib/pris/rates.rb
+++ lib/pris/rates.rb
@@ -4,11 +4,19 @@ module Pris
       define_method(direction) do |type = nil|
         rates = self[direction]
         if type
-          rates[type.to_s]
+          rates[type]
         else
           rates
         end
       end
+    end
+
+    def self.from_hash(hash)
+      output = self.new
+      hash.each do |(k,v)|
+        output[k] = v
+      end
+      output
     end
   end
 end
